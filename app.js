0f8cca47f4749bb02be5e43e4eaa23dcd6287bfa
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-11 21:56:52 +0530

    success

---
+++ app.js
@@ -0,0 +1,33 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const {coursemodel}=require("./models/course")
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://jinithajohnson:jingov02@cluster0.wo3ieyl.mongodb.net/courseDb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+    let input=req.body
+    let course=new coursemodel(input)
+    course.save()
+    console.log(course)
+    res.json({status:"success"})
+
+
+})
+app.get("/view",(req,res)=>{
+    coursemodel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch((error)=>{
+        res.json(error)
+    }
+)
+})
+
+app.listen(8080,()=>{
+    console.log("server started")
+})
---
+++ models/course.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose")
+const schema=mongoose.Schema(
+    {
+        "title":{type:String,required:true},
+        "description":{type:String,required:true},
+        "date":{type:String,required:true},
+        "duration":{type:String,required:true},
+        "venue":{type:String,required:true},
+        "tname":{type:String,required:true}
+
+    }
+)
+let coursemodel=mongoose.model("course",schema)
+module.exports={coursemodel}
