1bdb340fd9a9af24376bf9e7a82476ad13fe0ea1
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-12 13:32:51 +0530

    success

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const {coursemodel}=require("./models/course")
 const app=express()
 app.use(cors())
 app.use(express.json())
-
 mongoose.connect("mongodb+srv://jinithajohnson:jingov02@cluster0.wo3ieyl.mongodb.net/courseDb?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/add",(req,res)=>{
@@ -17,6 +16,21 @@ app.post("/add",(req,res)=>{
 
 
 })
+
+app.post("/search",(req,res)=>{
+    let input=req.body
+    coursemodel.find(input).then(
+        (data)=>{
+            res.json(data)
+        }
+        ).catch(
+            (error)=>{
+                res.json(error)
+            }
+)
+})
+
+
 app.get("/view",(req,res)=>{
     coursemodel.find().then(
         (data)=>{
@@ -27,6 +41,23 @@ app.get("/view",(req,res)=>{
     }
 )
 })
+
+app.post("/delete",(req,res)=>{
+    let input=req.body
+    coursemodel.findByIdAndDelete(input._id).then(
+        (response)=>{
+            res.json({"status":"success"})
+        }
+    ).catch(
+        (error)=>{
+            res.json({"status":"error"})
+        })
+        
+    })
+
+
+
+
 
 app.listen(8080,()=>{
     console.log("server started")
